f6995e29ecb685a00f05c46b56e0b50710681dd8
Yanaiara <yanairag@gmail.com>
2024-07-11 10:01:49 -0300

    Update Express app to include report route

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const { initDb } = require('./models');
 const authRoutes = require('./routes/authRoutes');
 const clockInRoutes = require('./routes/clockInRoutes');
+const reportRoutes = require('./routes/reportRoutes');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/api', clockInRoutes);
+app.use('/api', reportRoutes);
 
 const PORT = process.env.PORT || 5000;
 
