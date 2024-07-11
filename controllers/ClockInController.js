06542906fb3c2335d94e6f1f5201775d9643cc4e
Yanaiara <yanairag@gmail.com>
2024-07-10 21:58:57 -0300

    Update ClockIn controller to include clockOut functionality

--- controllers/ClockInController.js
+++ controllers/ClockInController.js
@@ -10,6 +10,17 @@ const clockIn = async (req, res) => {
   }
 };
 
+const clockOut = async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const clockOut = await ClockInService.updateClockOut({ userId });
+    res.status(200).json(clockOut);
+  } catch (error) {
+    res.status(500).json({ error: 'Clock-out failed' });
+  }
+};
+
 module.exports = {
-  clockIn
+  clockIn,
+  clockOut
 };
