dd48000942520efef0b8954f348ddc484b860048
Yanaiara <yanairag@gmail.com>
2024-07-10 21:59:44 -0300

    Update Express app to include clock-out endpoint

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
-app.use('/api', clockInRoutes); 
+app.use('/api', clockInRoutes);
 
 const PORT = process.env.PORT || 5000;
 
